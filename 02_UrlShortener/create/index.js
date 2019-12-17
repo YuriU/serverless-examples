99d5435d37a8bba6595db28d09c2a70468bffce8
YuriU <jerryhire8@gmail.com>
2019-12-17 23:02:45 +0200

    Added original url

--- 02_UrlShortener/create/index.js
+++ 02_UrlShortener/create/index.js
@@ -1,15 +1,19 @@
 'use strict';
 
+const queryString = require('querystring');
 const prefix = "mysite.com";
 
 module.exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
+    const submitted = queryString.parse(event.body).link;
+    console.log('Url submitted: ' + submitted);
+
     callback(null, {
         statusCode: 200,
         body: `
         <html>
             <body>
-                <h3>
+                <h3>URL ${submitted} has been shortend:
                     <a href="https://${prefix}/fake">${prefix}/fake</a>
                 </h3>
             </body>
