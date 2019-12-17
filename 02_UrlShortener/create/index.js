f392e249b4e4cd4f1f5f27510d6a153ff272ff09
YuriU <jerryhire8@gmail.com>
2019-12-17 22:58:20 +0200

    Added fake create lambda

---
+++ 02_UrlShortener/create/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const prefix = "mysite.com";
+
+module.exports.handler = (event, context, callback) => {
+    console.log(JSON.stringify(event));
+    callback(null, {
+        statusCode: 200,
+        body: `
+        <html>
+            <body>
+                <h3>
+                    <a href="https://${prefix}/fake">${prefix}/fake</a>
+                </h3>
+            </body>
+        </html>
+        `,
+        headers: { 'Content-Type' : 'text/html' }
+    });
+};
