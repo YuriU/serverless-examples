124dc427055c7f097b6c7f2c16a49ac61ac4dfa9
YuriU <jerryhire8@gmail.com>
2019-12-20 13:52:05 +0200

    Added dynamodb to create

--- 02_UrlShortener/create/index.js
+++ 02_UrlShortener/create/index.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const AWS = require('aws-sdk')
 const queryString = require('querystring');
+const path = require('path')
+const crypto = require('crypto')
 
+const docClient = new AWS.DynamoDB.DocumentClient()
+const tableName = `${process.env.SLS_STAGE}-shortened-urls`
+
+function RenderPage (link, submitted) {
+    return `
+  <html>
+  <body>
+  <h3>
+    <a href="${link}">${link}</a>
+  </h3>
+  <p>URL ${submitted} was shortened to:
+    <a href="${link}">${link}</a>
+  </p>
+  </body>
+  </html>`
+  }
 
 module.exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
@@ -9,17 +28,48 @@ module.exports.handler = (event, context, callback) => {
     console.log('Url submitted: ' + submitted);
     const prefix = event.headers.Referer || "mysite.com";
 
-    callback(null, {
-        statusCode: 200,
-        body: `
-        <html>
-            <body>
-                <h3>URL ${submitted} has been shortend:
-                    <a href="https://${prefix}/fake">${prefix}/fake</a>
-                </h3>
-            </body>
-        </html>
-        `,
-        headers: { 'Content-Type' : 'text/html' }
-    });
+    return new Promise((resolve, reject) => {
+        
+        resolve(crypto.randomBytes(8)
+                      .toString('base64')
+                      .replace(/[=+]/g, '')
+                      .substring(0, 4)
+               )
+    })
+    .then(slug => {
+        console.log(`Trying to save URL ${submitted} slug ${slug} now`);
+        return docClient.put({
+            TableName: tableName,
+            Item: {
+                slug: slug,
+                long_url: submitted
+            },
+            Expected: {
+                long_url: { Exists : false }
+            }
+
+        })
+        .promise().then(() => { return slug })
+    })
+    .then((slug) => {
+        console.log('woo, succeeded!');
+        return callback(
+            null,
+            {
+              statusCode: 200,
+              body: RenderPage(path.join(prefix, slug).replace(':/', '://'), prefix),
+              headers: {'Content-Type': 'text/html'}
+            }
+          );
+    })
+    .catch(error => {
+        console.log('Oh no, hit an error! ' + error)
+        callback(
+            null,
+            {
+              statusCode: 400,
+              body: 'Something went wrong, please try again'
+            }
+          );
+    })
 };
