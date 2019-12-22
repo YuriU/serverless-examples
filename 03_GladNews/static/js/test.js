d2923d2f40d50a56bfe7ab47c0a2b9197f1461d6
YuriU <jerryhire8@gmail.com>
2019-12-22 20:33:43 +0200

    Added mutators

--- 03_GladNews/static/js/test.js
+++ 03_GladNews/static/js/test.js
@@ -1,12 +1,25 @@
 const listArticlesQuery = `
 {
-    articles {
-        id,
-        author_id,
-        link
-    }
+  articles {
+    id,
+    author_id,
+    link
+  }
 }`
 
+const newArticleQuery = `
+mutation ($title: String!, $link: String!){
+  createArticle(article: {
+    link: $link,
+    title: $title
+  }) {
+    author_id,
+    id,
+    posted_at,
+    link,
+    title
+  }
+}`
 function testPage () {
   document.getElementById('buttonGet').addEventListener('click', () => {
     withToken().then(token => {
@@ -30,13 +43,24 @@ function testPage () {
 
   document.getElementById('buttonListArticles').addEventListener('click', () => {
     gqlQuery(listArticlesQuery, {}, true).then(respObject => {
-        document.getElementById('output').innerHTML = JSON.stringify(respObject, null, 2)
+      document.getElementById('output').innerHTML = JSON.stringify(respObject, null, 2)
     }).catch(err => {
-        console.log('Sad days: ' + err)
+      console.log('Sad days: ' + err)
     })
   })
 
   document.getElementById('buttonCreateArticle').addEventListener('click', () => {
-    // TODO
+    gqlQuery(
+      newArticleQuery,
+      {
+        title: 'Test Article',
+        link: 'https://read.acloud.guru/'
+      },
+      true
+    ).then(respObject => {
+      document.getElementById('output').innerHTML = JSON.stringify(respObject, null, 2)
+    }).catch(err => {
+      console.log('Sad days: ' + err)
+    })
   })
 }
