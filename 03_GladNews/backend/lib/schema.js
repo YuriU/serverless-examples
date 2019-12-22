508c8d04533362aaa381fa74a80fc652e330a6fd
YuriU <jerryhire8@gmail.com>
2019-12-22 19:37:41 +0200

    Fixing DynamoDB connection

--- 03_GladNews/backend/lib/schema.js
+++ 03_GladNews/backend/lib/schema.js
@@ -1,24 +1,22 @@
 'use strict'
 
 const G = require('graphql')
-const resolvers = require('./resolvers')
 const types = require('./types')
 
 const rootQuery = new G.GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      ping: {
-        type: G.GraphQLString,
-        // {"ping": "pong"}
-        resolve () {
-          return 'pong'
-        }
-      },
-      articles: types.Articles,
-      article: types.Article
-    }
-  })
+  name: 'RootQueryType',
+  fields: {
+    ping: {
+      type: G.GraphQLString,
+      resolve () {
+        return 'pong'
+      }
+    },
+    articles: types.Articles,
+    article: types.Article
+  }
+})
 
-module.exports.Schema = new G.GraphQLSchema( {
-    query: rootQuery
+module.exports.Schema = new G.GraphQLSchema({
+  query: rootQuery
 })
