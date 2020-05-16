0f0facb5811293464db247bfcadd40d3c178e230
YuriU <jerryhire8@gmail.com>
2020-05-16 21:24:36 +0300

    Added initial version of react app

---
+++ 06_Auth0/auth0-app-frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+      
+  }
+
+  render() {
+    return (<div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
