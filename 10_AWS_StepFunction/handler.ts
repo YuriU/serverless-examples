ca9c3872760a1e176ed77596b725a31cd66d119a
YuriU <jerryhire8@gmail.com>
2020-05-27 22:56:51 +0300

    Trying to trigger step function from lambda

--- 10_AWS_StepFunction/handler.ts
+++ 10_AWS_StepFunction/handler.ts
@@ -1,7 +1,38 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+var AWS = require('aws-sdk');
 import 'source-map-support/register';
 
+export const startExecution: APIGatewayProxyHandler = async(event, _context) => {
+  var stepfunctions = new AWS.StepFunctions({apiVersion: '2016-11-23'});
+
+  var params = {
+    stateMachineArn: process.env.STEP_FUNCTION_ARN,
+    input: 'STRING_VALUE',
+    name: 'STRING_VALUE'
+  };
+
+  var result = await stepfunctions.startExecution(params);
+
+  console.log("RESULT: >>>>");
+  console.log(result);
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Step function is launched',
+      input: event,
+    }, null, 2),
+  };
+}
+
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  var stepfunctions = new AWS.StepFunctions({apiVersion: '2016-11-23'});
+
+  console.log(stepfunctions);
+
+  
+  
   return {
     statusCode: 200,
     body: JSON.stringify({
