d5341720985831d5268717888ebdc78eb7f721b3
YuriU <jerryhire8@gmail.com>
2020-05-31 21:26:06 +0300

    Trying to launch stepfunctions

--- 10_AWS_StepFunction/handler.ts
+++ 10_AWS_StepFunction/handler.ts
@@ -1,14 +1,19 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 var AWS = require('aws-sdk');
 import 'source-map-support/register';
+import { v4 as uuidv4 } from 'uuid';
 
 export const startExecution: APIGatewayProxyHandler = async(event, _context) => {
   var stepfunctions = new AWS.StepFunctions({apiVersion: '2016-11-23'});
 
+  console.log(JSON.stringify(process.env.STEP_FUNCTION_ARN));
+
+  const transactionId = uuidv4();
+
   var params = {
     stateMachineArn: process.env.STEP_FUNCTION_ARN,
-    input: 'STRING_VALUE',
-    name: 'STRING_VALUE'
+    input:  JSON.stringify({ 'Message': 'HelloWorld' }),
+    name: transactionId
   };
 
   var result = await stepfunctions.startExecution(params);
