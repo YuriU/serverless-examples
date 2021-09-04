9a7928d9ac9793a158c6187a7059f8a08c394259
YuriU <jerryhire8@gmail.com>
2021-09-04 22:12:45 +0300

    Chaining example

--- 11_AWS_StepFunctionCourse/handler.js
+++ 11_AWS_StepFunctionCourse/handler.js
@@ -3,3 +3,12 @@
 module.exports.hello = async (name) => {
   return `hello ${name}`;
 };
+
+
+module.exports.add = async ({x, y}) => {
+  return x + y;
+};
+
+module.exports.double = async (n) => {
+  return n * 2;
+};
